1b19930572a53511f06637306f3a33e4ac86308a
Samba Sallah <sambasallah10@gmail.com>
2022-02-18 12:09:50 -0600

    modification to reverse

--- src/problem19/Solution.java
+++ src/problem19/Solution.java
@@ -10,6 +10,10 @@ public class Solution {
             int last = digit % 10;
             digit = digit / 10;
             reverse = reverse * 10 + last;
+
+            if(reverse > Integer.MAX_VALUE) return 0;
+
+            if(reverse < Integer.MIN_VALUE) return 0;
         }
 
         return reverse;
